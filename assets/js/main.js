d57e55ef067866d943c0ba8d0d4cdcf977a0c22b
Ian Noyes <ian@12southmusic.com>
2014-05-22 15:41:22 -0500

    syntax

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,7 @@ var Roots = {
       // JavaScript to be fired on the home page
       //Call fitVids on homepage
       $('.vid-home').fitVids();
-      $('.testimonial-container').flexslider(
+      $('.testimonial-container').flexslider({
     animation: "slide"
       });
       console.log('home call is firing');
