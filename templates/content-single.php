28eef8ed7ed2b81cb90b9bae8897004da94bb35d
Ian Noyes <ian@12southmusic.com>
2014-05-29 13:44:35 -0500

    only add header on single pages?

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,9 +1,12 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
+  <?php if (is_single) {
+    echo'
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
-    </header>
+    </header>'
+  } ?>
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
