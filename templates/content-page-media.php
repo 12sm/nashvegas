457d32ac16c4b6b8ae12c22f3551c97257ba4df5
Ian Noyes <ian@12southmusic.com>
2014-06-04 15:43:51 -0500

    media page template

--- templates/content-page-media.php
+++ templates/content-page-media.php
@@ -1,3 +1,11 @@
 <div class="row">
 	<div class="col-sm-4 col-offset-sm-4">
-		<img src="../img/media.png" />
+		<img src="../img/media.png" class="img-responsive"/>
+	</div>
+</div>
+<?php echo do_shortcode('[wp-view view-name="MEDIA | Videos"]'); ?>
+<div class="row image-slider">
+	<div class="col-sm-12 media-photos">
+		[photos gallery="test"]
+	</div>
+</div>
