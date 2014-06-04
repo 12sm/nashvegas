7e2dde173ee2ef929682f8a32dfd5c292d3721df
Ian Noyes <ian@12southmusic.com>
2014-06-04 17:08:46 -0500

    throw an error

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,6 +41,7 @@ var Roots = {
    //Media page
   page_id_17: {
     init: function() {
+      kljkl;jkl;jkl
       console.log('media scripts firing');
       $('.vid-container').fitVids();
       $('.slider').flexslider({
