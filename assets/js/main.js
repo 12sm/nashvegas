0eb2245b91b33915b4c9b63c58dc242ffdb9de6f
michael <michael@12southmusic.com>
2014-06-23 14:30:03 -0500

    Try to filter the song list

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,6 +79,38 @@ var Roots = {
     }
   },
 
+// Songlist page.
+  songlist: {
+    init: function() {
+      // JavaScript to be fired on the testimonials page
+
+      var $container = $('.songlist-body');
+      // init
+      $container.isotope({
+	      // options
+	      itemSelector: '.item',
+	      layoutMode: 'fitRows'
+	   });
+	   
+	   // bind filter button click
+	   $('#filters').on( 'click', 'button', function() {
+		   var filterValue = $( this ).attr('data-filter');
+		   // use filterFn if matches value
+		   filterValue = filterFns[ filterValue ] || filterValue;
+		   $container.isotope({ filter: filterValue });
+		});
+		// change is-checked class on buttons
+		$('.button-group').each( function( i, buttonGroup ) {
+			var $buttonGroup = $( buttonGroup );
+			$buttonGroup.on( 'click', 'button', function() {
+				$buttonGroup.find('.is-checked').removeClass('is-checked');
+				$( this ).addClass('is-checked');
+			});
+		});
+
+    }
+  },  
+
  
 };
 
